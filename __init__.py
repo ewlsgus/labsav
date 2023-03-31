fd6b9e3338309e03a6099a80625024412a95eba1
ewlsgus <ewlsgus@gmail.com>
2023-03-31 14:07:31 -0400

    directory restructuring

--- __init__.py
+++ __init__.py
@@ -6,6 +6,7 @@ sys.path.append(os.path.join(Path(__file__).resolve().parents[0], 'lib/bases'))
 from calculate import Calculate
 from analysis import Analyze
 from visualize import Visualize
+from utilities import Utilities
 
 if __name__ == "__main__":
     pass
--- lib/bases/utilities.py
+++ lib/bases/utilities.py
@@ -3,7 +3,7 @@ from pathlib import Path
 
 sys.path.append(os.path.join(Path(__file__).resolve().parents[1], 'utilities'))
 
-class Calculate():
+class Utilities():
     r'''Define useful utility functions'''
     def __init__(self):
         pass
